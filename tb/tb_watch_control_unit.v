84bf06608af01bdc162af58cd82677206cf21d0e
jy0917 <jiyoonirago0302@gmail.com>
2026-08-04 10:26:46 +0900

    fix : tb_watch_control_unit

--- tb/tb_watch_control_unit.v
+++ tb/tb_watch_control_unit.v
@@ -1,77 +1,89 @@
 `timescale 1ns / 1ps
+module tb_watch_and_stopwatch_top ();
+    parameter TICK_DELAY = 1_000_000 * 10;
 
-module tb_watch_control_unit ();
+    reg clk, reset;
+    reg btn_L, btn_R, btn_UP, btn_DOWN;
+    reg  [2:0] sw;
 
-    reg clk;
-    reg reset;
-    reg btn_L;
-    reg btn_R;
+    wire [3:0] fnd_com;
+    wire [7:0] fnd_data;
+    wire  led;
 
-    wire [1:0] state;
 
-    watch_control_unit U_WATCH_CNTL_UNIT (
-        .clk  (clk),
+    top_stopwatch dut (
+        .clk(clk),
         .reset(reset),
-        .btn_L(btn_L),
-        .btn_R(btn_R),
-        .state(state)
+        .btn_L(btn_L),  // runstop(s) / 자리변경(w) 
+        .btn_R(btn_R),  // clear(s) / 자리변경(w)
+        .btn_UP(btn_UP),  // mode(s) / up(w)
+        .btn_DOWN(btn_DOWN),  // option(s) / down(w)
+        .sw(sw),  // sw[0]: 0-stopwatch / 1:watch
+        .fnd_com(fnd_com),
+        .fnd_data(fnd_data),
+        .led(led)  // indicator
     );
 
     always #5 clk = ~clk;
 
     initial begin
-        clk   = 0;
+        clk = 0;
         reset = 1;
+        btn_L = 0;
         btn_R = 0;
+        btn_UP = 0;
+        btn_DOWN = 0;
+        sw = 3'b010; // watch 모드
+        #10; 
+        reset = 0;
+
+        #(TICK_DELAY*3); //btn_R 누름
+        btn_R = 1;
+        #(TICK_DELAY*3); 
+        btn_R = 0;
+
+        #(TICK_DELAY);
+        btn_R = 1;
+        #(TICK_DELAY*3); 
+        btn_R = 0;
+
+        #(TICK_DELAY);
+        btn_R = 1;
+        #(TICK_DELAY*3); 
+        btn_R = 0;
+
+        #(TICK_DELAY);
+        btn_R = 1;
+        #(TICK_DELAY*3); 
+        btn_R = 0;
+
+        #(TICK_DELAY);
+
+        #(TICK_DELAY); //btn_L 누름
+        btn_L = 1;
+        #(TICK_DELAY*3); 
         btn_L = 0;
 
-        #10 reset = 0;
+        #(TICK_DELAY);
+        btn_L = 1;
+        #(TICK_DELAY*3); 
+        btn_L = 0;
+
+        #(TICK_DELAY);
+        btn_L = 1;
+        #(TICK_DELAY*3); 
+        btn_L = 0;
+
+        #(TICK_DELAY);
+        btn_L = 1;
+        #(TICK_DELAY*3); 
+        btn_L = 0;
+
+        #(TICK_DELAY);
+        btn_L = 1;
         
-
-        #5 btn_R = 1;
-        #10 
-        btn_R = 0;
-        #10
-        btn_R = 1;
-
-        #10 
-        btn_R = 0;
-        #10
-        btn_R = 1;
-
-        #10
-        btn_R = 0;
-        #10
-        btn_R = 1;
-
-        #10
-        btn_R = 0;
-
-        btn_L = 1;
-        #10
-        btn_L = 0;
-        #10
-        btn_L = 1;
-         
-        #10
-        btn_L = 0;
-        #10
-        btn_L = 1;
-
-        #10
-        btn_L = 0;
-        #10
-        btn_L = 1;
-
-        #10
-        btn_L = 0;
-        #10
-        btn_L = 1;
-
-
+        
         $stop;
-
-
     end
 
 endmodule
